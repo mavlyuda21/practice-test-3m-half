5135276831e00abf74295e4f60f7ecb8fc60a865
mavlyuda <sashakilka1991.@gmail.com>
2023-03-18 20:36:49 +0600

    Initial Commit

---
+++ practice-test-3m-half/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice-test-3m-half
+//
+//  Created by mavluda on 18/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
